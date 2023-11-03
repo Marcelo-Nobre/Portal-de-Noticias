fcda4a878b2f029ab87c93e6686be820b3526ab7
Marcelo-Nobre <joaomarceloviana6@gmail.com>
2023-11-03 18:17:40 -0300

    adicionando tradução dinâmica en

--- lang/en/common.php
+++ lang/en/common.php
@@ -1,8 +1,15 @@
 <?php
 
 return [
+    'header' => [
+        'date' => ':weekday, :day, :month, :year',
+        'socialMedia' => 'Instagram’s big redesign goes live with',
+    ],
     'footer' => [
         'copyright' => '&copy; Copyright :year. All rights reserved.',
         'about' => 'About',
+        'descriptionForAbout' => 'News plays a vital role in our society, keeping us informed about local and global events that shape the world around us. They are a crucial source of knowledge, allowing us to make informed decisions and understand the issues that affect our lives.',
+        'topicForMostViewd' => 'Success is not a good teacher failure makes you humble',
+
     ],
 ];
